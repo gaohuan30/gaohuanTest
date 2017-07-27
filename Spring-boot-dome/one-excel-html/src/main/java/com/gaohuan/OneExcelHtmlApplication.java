193eadd83a2eab387ca0d6d0fd3c2a2de03e5d74
gaohuan30 <threadgaohuan@gmail.com>
2017-07-27 17:00:48 +0800

    spring-boot-dome

---
+++ Spring-boot-dome/one-excel-html/src/main/java/com/gaohuan/OneExcelHtmlApplication.java
@@ -0,0 +1,12 @@
+package com.gaohuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneExcelHtmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneExcelHtmlApplication.class, args);
+	}
+}
